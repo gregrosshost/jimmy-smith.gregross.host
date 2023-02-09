d78eb093b6694d84310c0dce769c9154935f5f76
Gregory Ross <gregross.dev@gmail.com>
2023-02-09 14:03:57 -0500

    entry single resource

--- app/Http/Controllers/Api/EntryController.php
+++ app/Http/Controllers/Api/EntryController.php
@@ -37,9 +37,9 @@ class EntryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Entry $entry)
     {
-        //
+       return new EntryResource($entry);
     }
 
     /**
