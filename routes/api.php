b5445f8794a8039185ad6ff5369eaea4af32bb78
Gregory Ross <gregross.dev@gmail.com>
2023-02-15 15:25:37 -0500

    date endpoint

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('dates', EntryController::class)->parameters(['entries' => 'entry:date']);
+Route::apiResource('dates', EntryController::class)->parameters(['dates' => 'entry:date']);
